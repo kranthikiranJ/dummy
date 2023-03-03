a0cbd60c1617ce6018f77d7354f653dd2032ba2d
kranthikiranJ <kranthikiran1993@gmail.com>
2023-03-03 13:15:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  });
+  const { username, email, password, confirmpassword } = data;
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://kranthikiran-91ad1-default-rtdb.firebaseio.com/register.json",
+        data
+      )
+      .then(() => alert("Submitted Successfully"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <form autoComplete="off" onSubmit={submitForm}>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            placeholder="username"
+            onChange={changeHandler}
+          />
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="email"
+            onChange={changeHandler}
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="password"
+            onChange={changeHandler}
+          />
+          <br />
+          <input
+            type="password"
+            name="confirmpassword"
+            value={confirmpassword}
+            placeholder="confirmpassword"
+            onChange={changeHandler}
+          />
+          <br />
+          <input type="submit" name="submit" />
+        </form>
+      </center>
     </div>
   );
-}
+};
 
 export default App;
